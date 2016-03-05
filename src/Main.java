fee9ce38005bb7c6a6cff2bc71b36855b9b8ea86
Chinh Nguyen <cqnguyen21st@yahoo.com>
2016-03-04 16:48:47 -0800

    Divide2

--- src/Main.java
+++ src/Main.java
@@ -14,14 +14,20 @@ public class Main {
 		int[][] input = {{0, 2, 3, 7}, {99, 0, 2, 4}, {99, 99, 0, 2}, {99, 99, 99, 0}};
 		dp = new int[input[0].length];
 		
-		long time1 = System.nanoTime();
-		System.out.println(BruteForce(input, 0, 4));
-		long time2 = System.nanoTime();
-		System.out.println(Dynamic(input, 0, 4));
-		long time3 = System.nanoTime();
-		System.out.println(time2 - time1);
-		System.out.println(time3 - time2);
+//		long time1 = System.nanoTime();
+//		System.out.println(BruteForce(input, 0, 4));
+//		long time2 = System.nanoTime();
+//		System.out.println(Dynamic(input, 0, 4));
+//		long time3 = System.nanoTime();
+//		System.out.println(time2 - time1);
+//		System.out.println(time3 - time2);
 //		System.out.println(DivideAndConquer(input, 0, 4));
+		
+		BestPath[] a = DivideAndConquer(input, 0, 4);
+		
+		for (BestPath b : a) {
+			System.out.println(b.cost);
+		}
 	}
 	
 	public static int BruteForce(int[][] input, int srs, int des) {
@@ -57,9 +63,15 @@ public class Main {
 	public static BestPath[] DivideAndConquer(int[][] input, int srs, int des) {
 		BestPath[] pathsCost = new BestPath[des - srs];
 		
+//		System.out.println(srs + " " + des);
+//		System.out.println(des/2);
+		
 		if (des - srs > 1 ) {
-			BestPath[] half1 = DivideAndConquer(input, srs, des/2);
-			BestPath[] half2 = DivideAndConquer(input, des/2, des);
+//			System.out.println(srs + " " + des/2);
+//			System.out.println(des/2 + " " + des);
+			
+			BestPath[] half1 = DivideAndConquer(input, srs, (des + srs) / 2);
+			BestPath[] half2 = DivideAndConquer(input, (des + srs) / 2, des);
 	
 			for (int i = 0; i < half1.length; i++) {
 				pathsCost[i] = half1[i];
@@ -67,43 +79,45 @@ public class Main {
 			
 			for (int i =  0; i < half2.length; i++) {
 //				BestPath bestOne = new BestPath();
-				pathsCost[des/2 + i] = half2[i];
-				if (pathsCost[des/2 + i].cost == 0) {
-					pathsCost[des/2 + i].cost = (int) Double.POSITIVE_INFINITY;
-				}
 				
-				for (int j = 0; j < half1.length; j++) {
-					BestPath currentPath = pathsCost[des/2 + i];
+				int d = half2[i].port;
+				half2[i].previous = null;
+				
+				for (int j = 0; j < half1.length + i ; j++) {
+					int s = pathsCost[j].port;
+					int cost =  pathsCost[j].cost + input[s][d];
 					
-					currentPath.paths.addAll(half1[i].paths);
+					if (d == 3 && s == 0) {
+						System.out.println("Here");
+					}
 					
-					int newCost = input[srs + j][des/2 + i];
-					
-					currentPath.cost = newCost + half1[i].cost + half2[j].cost;
-					
-					if (currentPath.cost < bestOne.cost) {
-						bestOne = currentPath;
+					if (half2[i].cost > cost || half2[i].previous == null) {
+						half2[i].previous = pathsCost[j];
+						half2[i].cost = cost;
 					}
 				}
+				
+				pathsCost[i + half1.length] = half2[i];
 			}
 			
 			
 		} else {
-			Set<Integer> oneSet = new HashSet<Integer>();
-			oneSet.add(srs);
-			pathsCost[0].paths = oneSet;
-			pathsCost[0].cost = input[srs][des-1];
+			pathsCost[0] = new BestPath();
+			pathsCost[0].port = srs;
 		}
 		
 		return pathsCost;
 	}
 	public static class BestPath {
-		Set<Integer> paths;
+		BestPath previous;
+		int port;
 		int cost;
 		
 		public BestPath() {
-			paths = new HashSet<Integer>();
-			cost = (int) Double.POSITIVE_INFINITY;
+			previous = null;
+			port = -1;
+//			cost = (int) Double.POSITIVE_INFINITY;
+			cost = 0;
 		}
 	}
 	
